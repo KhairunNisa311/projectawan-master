e8ff6cf3d5ab562a16b41884bd7c575c73b16c9c
KhairunNisa311 <ginazoom1@gmail.com>
2022-12-16 18:32:39 +0700

    login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -69,7 +69,7 @@
 
             </div>
             <div class="col-sm-6 px-0 d-none d-sm-block">
-                <img src="toko.jpg" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: right;">
+                {{-- <img src="toko.jpg" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: right;"> --}}
             </div>
         </div>
     </div>
